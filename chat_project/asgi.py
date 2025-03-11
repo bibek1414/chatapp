6e78d26d89aca3c1f9ae53f12c67ce78336644aa
bibek1414 <bbhattarai770@gmail.com>
2025-03-11 09:44:00 +0545

    asgi config

--- chat_project/asgi.py
+++ chat_project/asgi.py
@@ -1,13 +1,37 @@
+# import os
+# import django
+# from django.core.asgi import get_asgi_application
+# from channels.routing import ProtocolTypeRouter, URLRouter
+# from channels.auth import AuthMiddlewareStack
+# from django.urls import re_path
+# from chat import consumers
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_project.settings')
+# django.setup()
+# application = ProtocolTypeRouter({
+#     "http": get_asgi_application(),
+#     "websocket": AuthMiddlewareStack(
+#         URLRouter(
+#             [   
+#                 re_path(r'ws/chat/(?P<room_id>[^/]+)/$', consumers.ChatConsumer.as_asgi()),
+#             ]
+#         )
+#     ),
+# })
+
+
 import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_project.settings')
+
 import django
+django.setup()
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.urls import re_path
-from chat import consumers
+from chat import consumers  # Import consumers AFTER django.setup()
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_project.settings')
-django.setup()
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
